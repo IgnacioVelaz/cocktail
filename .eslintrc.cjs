c9df105fe69ce7038c3a9e2e247e8a9ef32e1bda
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2024-02-19 10:54:51 +0100

    chore: ESLint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "airbnb",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "react", "prettier", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
+    "no-console": "warn",
+    "import/extensions": "off",
+    "react/function-component-definition": "off",
+    "prefer-const": ["error", { destructuring: "all" }],
+    "prefer-arrow-callback": ["error", { allowNamedFunctions: false }],
+    "max-len": ["error", 140],
+    quotes: [2, "double", { avoidEscape: true }],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+  overrides: [
+    {
+      files: ["index.ts"],
+      rules: {
+        "import/prefer-default-export": "off",
+      },
+    },
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
